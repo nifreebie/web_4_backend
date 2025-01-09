ceb6b92a5f384f44a3aa288439923b4b6ce39e45
Nikita Isupov <145199579+nifreebie@users.noreply.github.com>
2025-01-10 01:12:32 +0300

    feat: add autogenerated OpenApi Swagger documentation

---
+++ src/main/java/web/web4_backend/SwaggerRedirectServlet.java
@@ -0,0 +1,17 @@
+package web.web4_backend;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/swagger")
+public class SwaggerRedirectServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.sendRedirect(request.getContextPath() + "/swagger-ui.html");
+    }
+}
